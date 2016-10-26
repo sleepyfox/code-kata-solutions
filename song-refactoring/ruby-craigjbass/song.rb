a16bfed0f318701c8687ef6147e1526f6af5cbcd
Craig Bass <craig@madetech.com>
2016-10-26 12:57:45 +0100

    refactor out first animal

--- song-refactoring/ruby-craigjbass/song.rb
+++ song-refactoring/ruby-craigjbass/song.rb
@@ -1,44 +1,48 @@
 class Song
+  def initialize(animal)
+    @animal = animal
+  end
+
   def sing
     <<-HEREDOC
-There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a #{@animal}.
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the spider to catch the #{@animal};
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a bird;
 How absurd to swallow a bird.
 She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the spider to catch the #{@animal};
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a cat;
 Fancy that to swallow a cat!
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the spider to catch the #{@animal};
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a dog;
 What a hog, to swallow a dog!
 She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the spider to catch the #{@animal};
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a cow;
 I don't know how she swallowed a cow!
 She swallowed the cow to catch the dog,
 She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the spider to catch the #{@animal};
+I don't know why she swallowed a #{@animal} - perhaps she'll die!
 There was an old lady who swallowed a horse...
 ...She's dead, of course!
     HEREDOC
   end
 end
 
-song = Song.new.sing
+song = Song.new('fly').sing
 puts song
 
--- song-refactoring/ruby-craigjbass/song_spec.rb
+++ song-refactoring/ruby-craigjbass/song_spec.rb
@@ -1,7 +1,11 @@
 require_relative 'song'
 RSpec.describe Song do
-  it 'should be the song' do
-    expected = <<-HEREDOC
+  subject { described_class.new(animal).sing }
+
+  context 'given first animal is fly' do
+    let(:animal) { 'fly' }
+    it 'the old lady should swallow a fly' do
+      expected = <<-HEREDOC
 There was an old lady who swallowed a fly.
 I don't know why she swallowed a fly - perhaps she'll die!
 There was an old lady who swallowed a spider;
@@ -36,7 +40,51 @@ She swallowed the spider to catch the fly;
 I don't know why she swallowed a fly - perhaps she'll die!
 There was an old lady who swallowed a horse...
 ...She's dead, of course!
-    HEREDOC
-    expect(described_class.new.sing).to eq(expected)
+      HEREDOC
+      is_expected.to eq(expected)
+    end
+  end
+
+  context 'given first animal is ant' do
+    let(:animal) { 'ant' }
+    it 'the old lady should swallow a ant' do
+      expected = <<-HEREDOC
+There was an old lady who swallowed a ant.
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a spider;
+That wriggled and wiggled and tickled inside her.
+She swallowed the spider to catch the ant;
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a bird;
+How absurd to swallow a bird.
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the ant;
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a cat;
+Fancy that to swallow a cat!
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the ant;
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a dog;
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat,
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the ant;
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a cow;
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the dog,
+She swallowed the dog to catch the cat,
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the ant;
+I don't know why she swallowed a ant - perhaps she'll die!
+There was an old lady who swallowed a horse...
+...She's dead, of course!
+      HEREDOC
+      is_expected.to eq(expected)
+    end
   end
 end
