bfa1f97d12d365b26e28d8944918567ea77b049b
Craig Bass <craig@madetech.com>
2016-10-26 13:07:54 +0100

    Deal with edge case and simply tests

--- song-refactoring/ruby-craigjbass/song.rb
+++ song-refactoring/ruby-craigjbass/song.rb
@@ -6,29 +6,29 @@ class Song
   def sing
     <<-HEREDOC
 There was an old lady who swallowed a #{first_animal}.
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
 She swallowed the spider to catch the #{first_animal};
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a bird;
 How absurd to swallow a bird.
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the #{first_animal};
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a cat;
 Fancy that to swallow a cat!
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the #{first_animal};
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a dog;
 What a hog, to swallow a dog!
 She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the #{first_animal};
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a cow;
 I don't know how she swallowed a cow!
 She swallowed the cow to catch the dog,
@@ -36,7 +36,7 @@ She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the #{first_animal};
-I don't know why she swallowed a #{first_animal} - perhaps she'll die!
+I don't know why she swallowed #{first_animal_indefinite_determiner} #{first_animal} - perhaps she'll die!
 There was an old lady who swallowed a horse...
 ...She's dead, of course!
     HEREDOC
@@ -45,10 +45,14 @@ There was an old lady who swallowed a horse...
   private
 
   def first_animal
-    @animal
+    @animal[:name]
+  end
+
+  def first_animal_indefinite_determiner
+    @animal[:indefinite_determiner]
   end
 end
 
-song = Song.new('fly').sing
+song = Song.new({ name: 'fly', indefinite_determiner: 'a' }).sing
 puts song
 
--- song-refactoring/ruby-craigjbass/song_spec.rb
+++ song-refactoring/ruby-craigjbass/song_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Song do
   subject { described_class.new(animal).sing }
 
   context 'given first animal is fly' do
-    let(:animal) { 'fly' }
+    let(:animal) { { indefinite_determiner: 'a', name: 'fly' } }
     it 'the old lady should swallow a fly' do
       expected = <<-HEREDOC
 There was an old lady who swallowed a fly.
@@ -46,45 +46,16 @@ There was an old lady who swallowed a horse...
   end
 
   context 'given first animal is ant' do
-    let(:animal) { 'ant' }
+    let(:animal) { { indefinite_determiner: 'an', name: 'ant' } }
     it 'the old lady should swallow a ant' do
-      expected = <<-HEREDOC
-There was an old lady who swallowed a ant.
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a spider;
-That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the ant;
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a bird;
-How absurd to swallow a bird.
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the ant;
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a cat;
-Fancy that to swallow a cat!
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the ant;
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a dog;
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the ant;
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a cow;
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the ant;
-I don't know why she swallowed a ant - perhaps she'll die!
-There was an old lady who swallowed a horse...
-...She's dead, of course!
-      HEREDOC
-      is_expected.to eq(expected)
+      is_expected.to include('There was an old lady who swallowed a ant.')
+      is_expected.to_not include('There was an old lady who swallowed a fly.')
+
+      is_expected.to include('She swallowed the spider to catch the ant;')
+      is_expected.to_not include('She swallowed the spider to catch the fly;')
+
+      is_expected.to include("I don't know why she swallowed an ant - perhaps she'll die!")
+      is_expected.to_not include("I don't know why she swallowed a fly - perhaps she'll die!")
     end
   end
 end
